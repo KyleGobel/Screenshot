25c7adefeaf9887758fcc2bcf222660feff34568
Kyle Gobel <kgobel@gmail.com>
2014-06-08 11:56:11 -0400

    moved phantom path

--- src/Screenshot/screenshot/tmp.js
+++ src/Screenshot/screenshot/tmp.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.open("http://www.pluralsight.com", function() {
-  page.render("screenshot/3b8d5746369a40d7a312c89b1ee829e6.png");
+page.open("http://www.google.com", function() {
+  page.render("screenshot/d1bf52a2f18b4c288ff6d6a6ed699639.png");
   phantom.exit();
 });
