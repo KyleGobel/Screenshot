15c57e002ae01ca84eca806270b4ca17f72388cc
Kyle Gobel <kgobel@gmail.com>
2014-06-09 19:59:08 -0400

    added login service

---
+++ src/Screenshot/AdminService.cs
@@ -0,0 +1,31 @@
+﻿using Screenshot.Models;
+using ServiceStack;
+using ServiceStack.OrmLite;
+
+namespace Screenshot
+{
+    public class AdminService : Service
+    {
+        public object Post(LoginRequest request)
+        {
+            if (Db.Exists<Admin>(x => x.Username == request.Username && x.Password == request.Password))
+                return new LoginResponse {Success = true};
+            else
+            {
+                return new LoginResponse {Success = false};
+            }
+        }
+    }
+
+    [Route("/login", "POST")]
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+    }
+}
---
+++ src/Screenshot/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.DataAnnotations;
+
+namespace Screenshot.Models
+{
+    [Schema("dbo")]
+    public class Admin
+    {
+        [Alias("admin_id"), PrimaryKey]
+        public string Username { get; set; }
+
+        [Alias("admin_pass")]
+        public string Password { get; set; }
+
+        [Alias("admin_level")]
+        public int? Level { get; set; }
+
+        [Alias("display_name")]
+        public string DisplayName { get; set; }
+         
+    }
+}
