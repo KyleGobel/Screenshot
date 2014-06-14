7dc59a1d3258fc1a7f6f029be77f0e6b01c0109a
Kyle Gobel <kgobel@gmail.com>
2014-06-14 14:45:42 -0400

    added a find links service

---
+++ src/Screenshot/LinkService.cs
@@ -0,0 +1,75 @@
+﻿using Screenshot.Models;
+using ServiceStack;
+using ServiceStack.OrmLite;
+
+namespace Screenshot
+{
+    public class LinkService : Service
+    {
+        public object Get(FindLinks request)
+        {
+            var jn = new JoinSqlBuilder<LinkEx, Link>();
+            jn = jn.Join<Link, Page>(x => x.PageId, x => x.Id, x => new {x.Id, x.Title, x.Url},
+                x => new {PageTitle = x.Title});
+
+            if (request.Id != default(int))
+            {
+                jn.Where<Link>(x => x.Id == request.Id);
+
+                var sql = jn.ToSql();
+                return Db.Select<LinkEx>(sql);
+            }
+
+            if (request.Title != default(string) && request.Url != default(string))
+            {
+                if (request.ExactMatch)
+                    jn.Where<Link>(x => x.Title == request.Title && x.Url == request.Url);
+                else
+                {
+                    jn.Where<Link>(x => x.Title.Contains(request.Title) && x.Url.Contains(request.Url));
+                }
+
+                var sql = jn.ToSql();
+                return Db.Select<LinkEx>(sql);
+            }
+
+            if (request.Title != default(string))
+            {
+                if (request.ExactMatch)
+                    jn.Where<Link>(x => x.Title == request.Title);
+                else
+                {
+                    jn.Where<Link>(x => x.Title.Contains(request.Title) );
+                }
+
+                var sql = jn.ToSql();
+                return Db.Select<LinkEx>(sql);
+            }
+
+            if (request.Url != default(string))
+            {
+                if (request.ExactMatch)
+                    jn.Where<Link>(x => x.Url == request.Url);
+                else
+                {
+                    jn.Where<Link>(x => x.Url.Contains(request.Url));
+                }
+
+                var sql = jn.ToSql();
+                return Db.Select<LinkEx>(sql);
+            }
+
+            return "nothing";
+        }
+         
+    }
+
+    [Route("/links", "GET")]
+    public class FindLinks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public bool ExactMatch { get; set; }
+    }
+}
---
+++ src/Screenshot/Models/Link.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using ServiceStack.DataAnnotations;
+
+namespace Screenshot.Models
+{
+    [Schema("dbo"), Alias("Links")]
+    public class Link
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public string Url { get; set; }
+
+        [Alias("Page_ID")]
+        public int PageId { get; set; }
+    }
+
+    [Schema("dbo"), Alias("Pages")]
+    public class Page
+    {
+        public int Id { get; set; }
+
+        [Alias("Folder_ID")]
+        public int FolderId { get; set; }
+        public string Title { get; set; }
+
+        [Alias("Auto_Ordering")]
+        public bool AutoOrdering { get; set; }
+
+    }
+
+    public class LinkEx
+    {
+        [BelongTo(typeof(Link))]
+        public int Id { get; set; }
+
+        [BelongTo(typeof(Link))]
+        public string Title { get; set; }
+
+        [BelongTo(typeof(Link))]
+        public string Url { get; set; }
+
+        [BelongTo(typeof(Page)), Alias("Title")]
+        public string PageTitle { get; set; }
+    }
+}
